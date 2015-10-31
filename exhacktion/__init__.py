ffa0cd1f05f5be0dcfb9a3e746eedf2d6f7b8a00
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2015-10-31 17:58:07 +0100

    Added some docstrings.

--- exhacktion/__init__.py
+++ exhacktion/__init__.py
@@ -1,3 +1,13 @@
+"""
+Exception matching patchwork.
+
+Libraries are not as fine-grained with exception classes as one would like. By
+using exhacktion, you can make create your own exceptions which behave as if
+they are superclasses of the ones raised by the library.
+
+Tread with caution.
+"""
+
 from exhacktion.base import (
     instance_checking_exception,
     message_checking_exception,
--- exhacktion/base.py
+++ exhacktion/base.py
@@ -7,6 +7,34 @@ import sys
 
 
 def instance_checking_exception(instance_checker):
+    """
+    Create an exception class which inspects the exception being raised.
+
+    This is most easily used as a decorator:
+
+    >>> @instance_checking_exception
+    ... def Foo(inst):
+    ...     return "Foo" in inst.message
+    >>> try:
+    ...     raise Exception("Something Fooish")
+    ... except Foo as e:
+    ...     print "True"
+    ... except Exception:
+    ...     print "False"
+    True
+
+    This is quite a powerful tool, mind you.
+
+    Arguments:
+        instance_checker (callable): A function which checks if the given
+            instance should be treated as an instance of a (subclass) of this
+            exception.
+
+    Returns:
+        Exception: (Actually: a new subclass of it), which calls the argument
+            `instance_checker` when it is checked against during exception
+            handling logic.
+    """
     class TemporaryClass(Exception):
         class __metaclass__(type):
             def __instancecheck__(cls, inst):
@@ -33,9 +61,33 @@ def instance_checking_exception(instance_checker):
 
 
 def message_checking_exception(regex, classname=None):
+    """
+    Create exception class which matches message against regular expression.
+
+    >>> Foo = message_checking_exception("Fooish", "Foo")
+    >>> Foo.__class__.__name__
+    'Foo'
+    >>> try:
+    ...     raise Exception("Something Fooish")
+    ... except Foo:
+    ...     print "True"
+    ... except Exception:
+    ...     print "False"
+    True
+
+    Arguments:
+        regex (string|RE): A regular expression which will be matched against
+            the `.message` attribute of the exception raised. Note that it uses
+            `re.search`, so if you want to match the beginning you have to
+            explicitly anchor the string (using `\A` or `^`).
+
+    Returns:
+        Exception: (Actually: a new subclass of it), which checks the message
+            of the exception against the supplied regex.
+    """
     @instance_checking_exception
     def check_message(inst):
         return re.search(regex, inst.message)
     if classname is not None:
-        check_message.__class__ = classname
+        check_message.__class__.__name__ = classname
     return check_message
