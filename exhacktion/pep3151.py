71bc999152aa88932f4d7a1c360555c0ac21a5fa
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2015-10-31 16:40:23 +0100

    Add docblocks.

--- exhacktion/pep3151.py
+++ exhacktion/pep3151.py
@@ -12,6 +12,7 @@ from exhacktion.base import instance_checking_exception
 
 @instance_checking_exception
 def BlockingIOError(inst):
+    """I/O operation would block."""
     errnos = {errno.EAGAIN, errno.EALREADY, errno.EWOULDBLOCK,
               errno.EINPROGRESS}
     return hasattr(inst, 'errno') and inst.errno in errnos
@@ -19,11 +20,13 @@ def BlockingIOError(inst):
 
 @instance_checking_exception
 def ChildProcessError(inst):
+    """Child process error."""
     return hasattr(inst, 'errno') and inst.errno == errno.ECHILD
 
 
 @instance_checking_exception
 def ConnectionError(inst):
+    """Connection error."""
     errnos = {errno.EPIPE, errno.ESHUTDOWN, errno.ECONNABORTED,
               errno.ECONNREFUSED, errno.ECONNRESET}
     return hasattr(inst, 'errno') and inst.errno in errnos
@@ -31,40 +34,48 @@ def ConnectionError(inst):
 
 @instance_checking_exception
 def FileExistsError(inst):
+    """File already exists."""
     return hasattr(inst, 'errno') and inst.errno == errno.EEXIST
 
 
 @instance_checking_exception
 def FileNotFoundError(inst):
+    """File not found."""
     return hasattr(inst, 'errno') and inst.errno == errno.ENOENT
 
 
 @instance_checking_exception
 def InterruptedError(inst):
+    """Interrupted by signal."""
     return hasattr(inst, 'errno') and inst.errno == errno.EINTR
 
 
 @instance_checking_exception
 def IsADirectoryError(inst):
+    """Operatino doesn't work on directories."""
     return hasattr(inst, 'errno') and inst.errno == errno.EISDIR
 
 
 @instance_checking_exception
 def NotADirectoryError(inst):
+    """Operation only works on directories."""
     return hasattr(inst, 'errno') and inst.errno == errno.ENOTDIR
 
 
 @instance_checking_exception
 def PermissionErrror(inst):
+    """Not enough permissions."""
     errnos = {errno.EACCES, errno.EPERM}
     return hasattr(inst, 'errno') and inst.errno in errnos
 
 
 @instance_checking_exception
 def ProcessLookupError(inst):
+    """Process not found."""
     return hasattr(inst, 'errno') and inst.errno == errno.ESRCH
 
 
 @instance_checking_exception
 def TimeoutError(inst):
+    """Timeout expired."""
     return hasattr(inst, 'errno') and inst.errno == errno.ETIMEDOUT
