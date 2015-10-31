d65cec91579c1bbe5b8db91519e4f3cd987f5341
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2015-10-31 13:27:49 +0100

    Implement the other exception classes mentioned in PEP3151.

--- exhacktion/pep3151.py
+++ exhacktion/pep3151.py
@@ -37,10 +37,60 @@ def instance_checking_exception(instance_checker):
 
 
 @instance_checking_exception
+def BlockingIOError(inst):
+    errnos = {errno.EAGAIN, errno.EALREADY, errno.EWOULDBLOCK,
+              errno.EINPROGRESS}
+    return hasattr(inst, 'errno') and inst.errno in errnos
+
+
+@instance_checking_exception
+def ChildProcessError(inst):
+    return hasattr(inst, 'errno') and inst.errno == errno.ECHILD
+
+
+@instance_checking_exception
+def ConnectionError(inst):
+    errnos = {errno.EPIPE, errno.ESHUTDOWN, errno.ECONNABORTED,
+              errno.ECONNREFUSED, errno.ECONNRESET}
+    return hasattr(inst, 'errno') and inst.errno in errnos
+
+
+@instance_checking_exception
+def FileExistsError(inst):
+    return hasattr(inst, 'errno') and inst.errno == errno.EEXIST
+
+
+@instance_checking_exception
 def FileNotFoundError(inst):
-    return isinstance(inst, (IOError, OSError)) and inst.errno == errno.ENOENT
+    return hasattr(inst, 'errno') and inst.errno == errno.ENOENT
+
+
+@instance_checking_exception
+def InterruptedError(inst):
+    return hasattr(inst, 'errno') and inst.errno == errno.EINTR
+
+
+@instance_checking_exception
+def IsADirectoryError(inst):
+    return hasattr(inst, 'errno') and inst.errno == errno.EISDIR
 
 
 @instance_checking_exception
 def NotADirectoryError(inst):
-    return isinstance(inst, (IOError, OSError)) and inst.errno == errno.ENOTDIR
+    return hasattr(inst, 'errno') and inst.errno == errno.ENOTDIR
+
+
+@instance_checking_exception
+def PermissionErrror(inst):
+    errnos = {errno.EACCES, errno.EPERM}
+    return hasattr(inst, 'errno') and inst.errno in errnos
+
+
+@instance_checking_exception
+def ProcessLookupError(inst):
+    return hasattr(inst, 'errno') and inst.errno == errno.ESRCH
+
+
+@instance_checking_exception
+def TimeoutError(inst):
+    return hasattr(inst, 'errno') and inst.errno == errno.ETIMEDOUT
--- tests/test_pep3151_emulation.py
+++ tests/test_pep3151_emulation.py
@@ -1,7 +1,9 @@
 import errno
 import os
+import re
 import unittest
 
+import exhacktion.pep3151
 from exhacktion.pep3151 import FileNotFoundError, NotADirectoryError
 
 
@@ -40,3 +42,15 @@ class TestPEP3151LikeSuperClassing(unittest.TestCase):
             pass
         except Exception as e:
             raise AssertionError("Could not create proper exception:" + str(e))
+
+    def test_all_errnos_defined(self):
+        # An extra sanity check against typos in the errno definitions.
+        path = exhacktion.pep3151.__file__
+        if path.endswith("pyc"):
+            path = path[:-1]
+
+        with open(path, 'r') as fp:
+            contents = fp.read()
+
+        for match in re.finditer(r"\berrno\.([A-Z]+)\b", contents):
+            assert hasattr(errno, match.group(1)), match.group(1)
